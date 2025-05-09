cfe778d476b5dd67918cb8989747625e8112d704
aspart-am <florian.esparsa@gmail.com>
2025-05-09 19:32:06 +0200

    Refactorisation des types d'action dans le composant use-toast : simplification en remplaçant l'utilisation d'un type ActionType par des chaînes de caractères directes. Amélioration de la lisibilité du code.

--- aci-app/components/ui/use-toast.ts
+++ aci-app/components/ui/use-toast.ts
@@ -15,20 +15,6 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-type ActionType = {
-  ADD_TOAST: "ADD_TOAST"
-  UPDATE_TOAST: "UPDATE_TOAST"
-  DISMISS_TOAST: "DISMISS_TOAST"
-  REMOVE_TOAST: "REMOVE_TOAST"
-}
-
-const actionTypes: ActionType = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const
-
 let count = 0
 
 function genId() {
@@ -38,19 +24,19 @@ function genId() {
 
 type Action =
   | {
-      type: ActionType["ADD_TOAST"]
+      type: "ADD_TOAST"
       toast: ToasterToast
     }
   | {
-      type: ActionType["UPDATE_TOAST"]
+      type: "UPDATE_TOAST"
       toast: Partial<ToasterToast>
     }
   | {
-      type: ActionType["DISMISS_TOAST"]
+      type: "DISMISS_TOAST"
       toastId?: ToasterToast["id"]
     }
   | {
-      type: ActionType["REMOVE_TOAST"]
+      type: "REMOVE_TOAST"
       toastId?: ToasterToast["id"]
     }
 
