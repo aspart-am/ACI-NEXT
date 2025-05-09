a0b0ab016c568c0ee6b227b056c452d0d0787e4c
aspart-am <florian.esparsa@gmail.com>
2025-05-10 00:05:44 +0200

    Modification du fichier package-lock.json pour mettre à jour les versions des dépendances, y compris la mise à jour de React et Next.js, ainsi que l'ajout de nouvelles dépendances. Refactorisation de la gestion des cookies dans le fichier server.ts pour utiliser des options de cookie spécifiques et améliorer la gestion des cookies dans le contexte serveur.

--- aci-app/app/lib/supabase/server.ts
+++ aci-app/app/lib/supabase/server.ts
@@ -1,26 +1,26 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import type { CookieOptions } from '@supabase/ssr';
 
 // Crée une instance Supabase pour les composants serveur
 export function createServerSupabaseClient() {
-  const cookieStore = cookies();
-  
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          // Utiliser la méthode synchrone pour obtenir le cookie
-          return cookieStore.get(name)?.value ?? '';
+          return cookies().get(name)?.value ?? '';
         },
-        set() {
-          // Dans un contexte serveur, on ne peut pas toujours modifier les cookies
-          // Cette fonction n'est pas implémentée car les cookies sont en lecture seule
+        set(name: string, value: string, options: CookieOptions) {
+          cookies().set({
+            name,
+            value,
+            ...options,
+          });
         },
-        remove() {
-          // Dans un contexte serveur, on ne peut pas toujours modifier les cookies
-          // Cette fonction n'est pas implémentée car les cookies sont en lecture seule
+        remove(name: string, options: CookieOptions) {
+          cookies().delete(name);
         },
       },
     }
