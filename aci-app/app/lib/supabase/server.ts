e33ca77b8d3547142d7166ce2581ddf95c7cef8e
aspart-am <florian.esparsa@gmail.com>
2025-05-10 00:13:06 +0200

    Refactorisation de la gestion des cookies dans le fichier server.ts pour utiliser cookies().getAll() et améliorer la récupération, la définition et la suppression des cookies, renforçant ainsi la maintenabilité du code.

--- aci-app/app/lib/supabase/server.ts
+++ aci-app/app/lib/supabase/server.ts
@@ -1,29 +1,29 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import type { CookieOptions } from '@supabase/ssr';
+import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
 // Crée une instance Supabase pour les composants serveur
 export function createServerSupabaseClient() {
-  const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          const cookie = cookieStore.get(name);
+          const allCookies = cookies().getAll();
+          const cookie = allCookies.find((cookie: RequestCookie) => cookie.name === name);
           return cookie?.value ?? '';
         },
         set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({
+          cookies().set({
             name,
             value,
             ...options,
           });
         },
         remove(name: string, _options: CookieOptions) {
-          cookieStore.delete(name);
+          cookies().delete(name);
         },
       },
     }
